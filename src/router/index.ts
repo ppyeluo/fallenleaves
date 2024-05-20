8e51c34194b4ac423b5ed9322f68fe5593ecf697
ppyeluo <3184182513@qq.com>
2024-05-20 10:22:32 +0800

    guess:首屏加载时加载进度条第一次未加载完就消失（elementPlus按需加载导致服务重启）

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,6 @@ import routes from './routes'
 import setting from '@/setting'
 import MyMessage from '@/utils/myMessage'
 import { GET_TOKEN } from '@/utils/token'
-import nprogress from 'nprogress'
-import 'nprogress/nprogress.css';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -13,13 +11,10 @@ const router = createRouter({
         return { top: 0 }
     }
 })
-// 关闭加载旋转器
-nprogress.configure({ showSpinner: false })
 // 全局前置守卫
 router.beforeEach((to, from, next) => {  
     if(to.meta.needToken){
         if(GET_TOKEN()){
-            nprogress.start();
             next()
         }else{
             MyMessage({
@@ -29,7 +24,6 @@ router.beforeEach((to, from, next) => {
             next(from)
         }
     }else{
-        nprogress.start();
         next()
     }
 })
@@ -37,7 +31,6 @@ router.beforeEach((to, from, next) => {
 // 全局后置守卫，切换路由成功后，改变页签标题
 router.afterEach((to, _from) => {
     document.title = `${setting.title}-${to.meta.title}`
-    nprogress.done();
 })
 
 export default router
